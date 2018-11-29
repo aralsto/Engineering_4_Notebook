b03c232adcdd19605b2c7da282f6d4b7af017ea6
aralsto <shelledreptilian42@gmail.com>
2018-11-29 12:37:02 -0500

    Finished LED function of flask_gpio, working on chaning the background based on LEDs.

--- Python/Flask/flask_gpio/app.py
+++ Python/Flask/flask_gpio/app.py
@@ -2,24 +2,33 @@ from flask import Flask, render_template, request
 import RPi.GPIO as GPIO
 
 led1 = None
+led2 = None
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17,GPIO.OUT)
+GPIO.setup(22,GPIO.OUT)
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET","POST"])
 def index():
 	global led1
+	global led2
 	if request.method == "POST":
 		if request.form.get("button1") == "on":
 			led1 = not led1
+		if request.form.get("button2") == "on":
+			led2 = not led2
 
-	if(led1):
+	if led1:
 		GPIO.output(17,GPIO.HIGH)
 	else:
 		GPIO.output(17,GPIO.LOW)
+	if led2:
+		GPIO.output(22,GPIO.HIGH)
+	else:
+		GPIO.output(22,GPIO.LOW)
 
 	return render_template("index.html")
 
